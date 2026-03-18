29841dd748d9812ebe28216a872e0b2d012a6b3f
ToddynhoVT9 <mateuslopes9954@gmail.com>
2026-03-18 17:40:07 -0300

    Repositorio zerado

--- forum/admin.py
+++
@@ -1,7 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import Topic, Thread, Post
-
-admin.site.register(Topic)
-admin.site.register(Thread)
-admin.site.register(Post)
